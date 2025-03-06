e553d2ecb7ddc4f4758de932e240167729b59a42
Petromir Kolev <petromirkolev@Petromirs-MacBook-Pro.local>
2025-03-06 21:52:17 +0200

    Work on game logic

--- script.js
+++ script.js
@@ -1,59 +1,24 @@
-// let gameChoices = ['rock', 'paper', 'scissors'];
-// let computerScore = 0;
-// let playerScore = 0;
-// let gameRounds = 0;
+let gameChoices = ['rock', 'paper', 'scissors'];
+let maxRounds;
+let currentRound = 1;
+let gameRounds = 0;
+let computerScore = 0;
+let playerScore = 0;
 
-// function computerChoice() {
-//   let computerPick = Math.floor(Math.random() * gameChoices.length);
-//   return gameChoices[computerPick];
-// }
+// Set Playground
+(() => {
+  let maxRounds = prompt('How many rounds would you like to play?');
 
-// function playerChoice() {
-//   let playerPick = prompt('Choose your weapon - rock, paper or scissors!');
-//   return playerPick;
-// }
+  document.getElementById(
+    'max-rounds'
+  ).textContent = `Max rounds: ${maxRounds}`;
+  document.getElementById(
+    'current-round'
+  ).textContent = `Current round: ${currentRound}`;
+  document.getElementById('winner').textContent = `Start the game!`;
+  document.getElementById('winner').style.backgroundColor = 'red';
+})();
 
-// function gamePlay() {
-//   let computer = computerChoice();
-//   let player = playerChoice();
-
-//   if (player !== 'rock' && player !== 'paper' && player !== 'scissors') {
-//     console.log(
-//       'Incorrect player input! Please pick between rock, paper or scissors only!'
-//     );
-//     return;
-//   }
-
-//   if (computer === 'rock' && player === 'scissors') {
-//     computerScore++;
-//     alert('Computer wins!');
-//     gamePlay();
-//   } else if (computer === 'paper' && player === 'rock') {
-//     computerScore++;
-//     alert('Computer wins!');
-//     gamePlay();
-//   } else if (computer === 'scissors' && player === 'paper') {
-//     computerScore++;
-//     alert('Computer wins!');
-//     gamePlay();
-//   } else if (computer === player) {
-//     alert("It's a tie!");
-//     return;
-//   } else {
-//     playerScore++;
-//     alert('Player wins!');
-//   }
-//   gameRounds++;
-// }
-
-// while (gameRounds < 5) {
-//   gamePlay();
-// }
-
-// computerScore > playerScore
-//   ? alert(
-//       `Computer wins! Computer score = ${computerScore} Player score = ${playerScore}`
-//     )
-//   : alert(
-//       `Player wins! Player score = ${playerScore} Player score = ${computerScore}`
-//     );
+function computerChoice() {
+  return gameChoices[Math.floor(Math.random() * gameChoices.length)];
+}
